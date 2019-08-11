e019ad3b3b8f2776bf974a43ec203296e241f27b
robin <robin@shroomlife.de>
2019-08-11 14:43:31 +0200

    added features

--- src/App.js
+++ src/App.js
@@ -7,7 +7,19 @@ import { Form, Input } from '@rocketseat/unform';
 
 import 'bootstrap/dist/css/bootstrap.css';
 
-import { IoIosArrowBack } from 'react-icons/io';
+import { MdArrowBack } from 'react-icons/md';
+
+import 'jquery';
+import 'popper.js';
+import 'bootstrap/dist/js/bootstrap.min';
+
+import Moment from 'react-moment';
+import 'moment-timezone';
+
+const DEFAULT_STATE = {
+  lists: [],
+  counter: 0
+};
 
 const DEFAULT_LIST = {
   id: false,
@@ -20,25 +32,6 @@ const DEFAULT_ITEM = {
   ticks: []
 };
 
-class Index extends React.Component {
-
-  constructor(props) {
-    super(props);
-  }
-
-  componentDidMount() {
-    console.log(this.props);
-  }
-
-  render() {
-    console.log(this.state);
-    return (
-      <div>Hi</div>
-    );
-  }
-
-}
-
 export default class App extends React.Component {
 
   constructor(props) {
@@ -46,45 +39,62 @@ export default class App extends React.Component {
 
     this.handleAddList = this.handleAddList.bind(this);
     this.handleAddItem = this.handleAddItem.bind(this);
+    this.handleTickUp = this.handleTickUp.bind(this);
+    this.resetAll = this.resetAll.bind(this);
 
     let loadedData = localStorage.getItem('STORAGE');
 
     if (loadedData) {
       this.state = JSON.parse(loadedData);
     } else {
-
-      this.state = {
-        lists: [],
-        counter: 0
-      };
-
+      this.state = DEFAULT_STATE;
     }
 
   }
 
-  handleAddList(data) {
-
-    console.log(data);
+  handleAddList(data, form) {
 
     let currentState = this.state;
-    let currentCounterValue = currentState.counter;
     currentState.counter += 1;
 
-    let newList = DEFAULT_LIST;
+    let newList = Object.assign({}, DEFAULT_LIST);
     newList.id = currentState.counter;
     newList.name = data.name;
 
     currentState.lists.push(newList);
-    this.setState(newList);
 
-    this.upload();
+    this.setState(newList, this.upload);
+    form.resetForm();
 
   }
 
-  handleAddItem(data) {
+  handleAddItem(data, form) {
 
-    console.log(data);
+    let currentState = this.state;
 
+    let newItem = Object.assign({}, DEFAULT_ITEM);
+    newItem.name = data.name;
+
+    console.log(newItem);
+
+    let itemKey = parseInt(data.id) - 1;
+    currentState.lists[itemKey].items.push(newItem);
+
+    console.log(itemKey);
+
+    this.setState(currentState, this.upload);
+    form.resetForm();
+
+  }
+
+  handleTickUp(conf) {
+    let currentState = this.state;
+    currentState.lists[conf.list-1].items[conf.id].ticks.push(1);
+    this.setState(currentState, this.upload);
+  }
+
+  resetAll() {
+    this.setState(DEFAULT_STATE, this.upload);
   }
 
   upload() {
@@ -97,6 +107,18 @@ export default class App extends React.Component {
       <div>
         <nav className="navbar navbar-light bg-light">
           <a className="navbar-brand" href="/">Örwilist</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <button type="button" className="btn btn-link" onClick={this.resetAll}>Alles zurücksetzen</button>
+              </li>
+            </ul>
+          </div>
+
         </nav>
 
         <div className="container">
@@ -118,10 +140,9 @@ export default class App extends React.Component {
                   </div>
                   <Form onSubmit={this.handleAddList} className="form-inline">
                     <div className="form-group">
-                      <label htmlFor="addNewListInput">List Name</label>
-                      <Input id="addNewListInput" name="name" />
+                      <Input className="form-control form-control-lg" id="addNewListInput" name="name" />
                     </div>
-                    <button type="submit" className="btn btn-primary">Add</button>
+                    <button type="submit" className="btn btn-primary btn-lg">Add</button>
                   </Form>
                 </div>
               );
@@ -130,32 +151,10 @@ export default class App extends React.Component {
             <Route path="/list/:id" exact render={props => {
 
               let selectedItemId = parseInt(props.match.params.id);
+              let selectedItem = this.state.lists[selectedItemId - 1];
 
               return (
-                <div className="listPage">
-                  {() => {
-
-                    let currentItem = findObjectByKey(this.state.lists, 'id', selectedItemId);
-                    return (
-                      <div>
-                        <h2>
-                          <Link to="/">
-                            <IoIosArrowBack />
-                          </Link>
-                          {currentItem.name}
-                        </h2>
-                        <Form onSubmit={this.handleAddItem} className="form-inline">
-                          <div className="form-group">
-                            <label htmlFor="addNewItemInput">Item Name</label>
-                            <Input id="addNewItemInput" name="name" />
-                          </div>
-                          <button type="submit" className="btn btn-primary">Add</button>
-                        </Form>
-                      </div>
-                    );
-
-                  }}
-                </div>
+                <List item={selectedItem} handleAddItem={this.handleAddItem} handleTickUp={this.handleTickUp} />
               );
 
             }} />
@@ -167,11 +166,51 @@ export default class App extends React.Component {
   }
 }
 
-function findObjectByKey(array, key, value) {
-  for (var i = 0; i < array.length; i++) {
-    if (array[i][key] === value) {
-      return array[i];
-    }
+class List extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = this.props.item;
   }
-  return null;
+
+  render() {
+
+    return (
+      <div>
+        <h2>
+          <Link to="/">
+            <MdArrowBack />
+          </Link>
+          <span>{this.state.name}</span>
+        </h2>
+        <div className="ticks">
+          {this.state.items.map((tickItem, i) => {
+
+            return (
+              <div key={i} className="tickItem" onClick={() => {
+                this.props.handleTickUp({
+                  item: tickItem,
+                  id: i,
+                  list: this.state.id
+                });
+              }}>
+                <span>{tickItem.name}</span>
+                <span>{tickItem.ticks.length}</span>
+              </div>
+            );
+
+          })}
+        </div>
+        <Form className="form-inline" onSubmit={this.props.handleAddItem}>
+          <Input type="hidden" name="id" value={this.state.id} />
+          <div className="form-group">
+            <Input className="form-control form-control-lg" id="addNewListInput" name="name" />
+          </div>
+          <button type="submit" className="btn btn-primary btn-lg">Add</button>
+        </Form>
+      </div>
+    );
+
+  }
+
 }
