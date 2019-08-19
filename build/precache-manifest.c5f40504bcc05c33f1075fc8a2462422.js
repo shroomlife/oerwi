0e305358f69015cac429e19c436505855e4fb57b
robin <robin@shroomlife.de>
2019-08-19 17:54:15 +0200

    added build directory to git

---
+++ build/precache-manifest.c5f40504bcc05c33f1075fc8a2462422.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed0fc20e686277b2c483965b2912a266",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67a3e09b3b134d6286bd",
+    "url": "/runtime~main.js"
+  },
+  {
+    "revision": "c068bc7b6ae42b375e9a",
+    "url": "/static/css/2.301a8994.chunk.css"
+  },
+  {
+    "revision": "0754c2b5c062c7bdafc5",
+    "url": "/static/css/main.a66260fd.chunk.css"
+  },
+  {
+    "revision": "c068bc7b6ae42b375e9a",
+    "url": "/static/js/2.c2d6cab2.chunk.7d8d5530-c299-11e9-85a8-037d9ef4ec6b.js"
+  },
+  {
+    "revision": "0754c2b5c062c7bdafc5",
+    "url": "/static/js/main.bec5a89e.chunk.7d8d5530-c299-11e9-85a8-037d9ef4ec6b.js"
+  }
+]);
