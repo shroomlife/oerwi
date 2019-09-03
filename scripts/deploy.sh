7a8ecfc7b960ccb540e6e1530b9afb2661db7cf1
robin <robin@shroomlife.de>
2019-09-03 15:36:04 +0200

    added build fixe

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-DEPLOY_BRANCH="$1"
-echo "DEPLOY BRANCH NAME: $DEPLOY_BRANCH"
-docker build . shroomlife/oerwi:$DEPLOY_BRANCH
+export DEPLOY_BRANCH="$1"
+docker build --tag shroomlife/oerwi:$DEPLOY_BRANCH .
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push shroomlife/oerwi:${DEPLOY_BRANCH}
