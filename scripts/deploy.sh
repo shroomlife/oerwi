771c63b39df2cab9c39d32bb29a8e8b815cdc5fe
robin <robin@shroomlife.de>
2019-09-03 15:40:49 +0200

    fixed automated build config

--- scripts/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export DEPLOY_BRANCH="$1"
-docker build --tag shroomlife/oerwi:$DEPLOY_BRANCH .
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push shroomlife/oerwi:${DEPLOY_BRANCH}
