f127ff204be39e950a1b2b8063a6b55885e94764
robin <robin@shroomlife.de>
2020-02-25 17:11:27 +0100

    upgraded dependencies for more security

--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -1,4 +1,4 @@
-'use strict';
+
 
 const errorOverlayMiddleware = require('react-dev-utils/errorOverlayMiddleware');
 const evalSourceMapMiddleware = require('react-dev-utils/evalSourceMapMiddleware');
@@ -98,7 +98,7 @@ module.exports = function(proxy, allowedHost) {
       // We do this in development to avoid hitting the production cache if
       // it used the same host and port.
       // https://github.com/facebook/create-react-app/issues/2272#issuecomment-302832432
-      app.use(noopServiceWorkerMiddleware());
+      app.use(noopServiceWorkerMiddleware(''));
     },
   };
 };
